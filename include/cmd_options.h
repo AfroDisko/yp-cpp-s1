0b3f82830eef5aca1f20deb8ebeba5b07c790b9b
AfroDisko <calexei76@gmail.com>
2025-05-10 14:09:08 +0000

    crypto guard

--- include/cmd_options.h
+++ include/cmd_options.h
@@ -2,35 +2,32 @@
 
 #include <boost/program_options.hpp>
 #include <string>
-#include <unordered_map>
 
 namespace CryptoGuard {
 
 class ProgramOptions {
 public:
-    ProgramOptions();
-    ~ProgramOptions();
+    enum class COMMAND_TYPE { NONE = -1, ENCRYPT, DECRYPT, CHECKSUM, COUNT };
 
-    enum class COMMAND_TYPE {
-        ENCRYPT,
-        DECRYPT,
-        CHECKSUM,
-    };
+    ProgramOptions();
+    ~ProgramOptions() = default;
 
     bool Parse(int argc, char *argv[]);
 
     COMMAND_TYPE GetCommand() const { return command_; }
-    std::string GetInputFile() const { return inputFile_; }
-    std::string GetOutputFile() const { return outputFile_; }
-    std::string GetPassword() const { return password_; }
+    const std::string &GetInputFile() const { return inputFile_; }
+    const std::string &GetOutputFile() const { return outputFile_; }
+    const std::string &GetPassword() const { return password_; }
 
 private:
-    COMMAND_TYPE command_;
-    const std::unordered_map<std::string_view, COMMAND_TYPE> commandMapping_ = {
-        {"encrypt", ProgramOptions::COMMAND_TYPE::ENCRYPT},
-        {"decrypt", ProgramOptions::COMMAND_TYPE::DECRYPT},
-        {"checksum", ProgramOptions::COMMAND_TYPE::CHECKSUM},
-    };
+    void SetupOptions();
+
+    void NotifierCommand(std::string);
+    void NotifierInputFile(std::string);
+    void NotifierOutputFile(std::string);
+    void NotifierPassword(std::string);
+
+    COMMAND_TYPE command_ = COMMAND_TYPE::NONE;
 
     std::string inputFile_;
     std::string outputFile_;
--- include/crypto_guard_ctx.h
+++ include/crypto_guard_ctx.h
@@ -1,13 +1,15 @@
 #pragma once
 
+#include <experimental/propagate_const>
+#include <memory>
 #include <string>
 
 namespace CryptoGuard {
 
 class CryptoGuardCtx {
 public:
-    CryptoGuardCtx() {}
-    ~CryptoGuardCtx() {}
+    CryptoGuardCtx();
+    ~CryptoGuardCtx();
 
     CryptoGuardCtx(const CryptoGuardCtx &) = delete;
     CryptoGuardCtx &operator=(const CryptoGuardCtx &) = delete;
@@ -15,14 +17,13 @@ public:
     CryptoGuardCtx(CryptoGuardCtx &&) noexcept = default;
     CryptoGuardCtx &operator=(CryptoGuardCtx &&) noexcept = default;
 
-    // API
-    void EncryptFile(std::iostream &inStream, std::iostream &outStream, std::string_view password) {}
-    void DecryptFile(std::iostream &inStream, std::iostream &outStream, std::string_view password) {}
-    std::string CalculateChecksum(std::iostream &inStream) { return "NOT_IMPLEMENTED"; }
+    void EncryptFile(std::istream &, std::ostream &, std::string_view /* password */);
+    void DecryptFile(std::istream &, std::ostream &, std::string_view /* password */);
+    std::string CalculateChecksum(std::istream &);
 
 private:
     class Impl;
-    Impl *pImpl_;
+    std::experimental::propagate_const<std::unique_ptr<Impl>> pImpl_;
 };
 
 }  // namespace CryptoGuard
